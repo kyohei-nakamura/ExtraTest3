c1f5d6ffb45fcf32c4bee4359f229c0e02471607
Eiji Takahashi <tak7iji@gmail.com>
2014-08-27 17:37:35 +0900

    locale変更サンプル追加

---
+++ src/main/java/org/sample/springmvc/extra/ExtraTest2.java
@@ -0,0 +1,39 @@
+package org.sample.springmvc.extra;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ExtraTest2 {
+    @Resource
+    UserInfoValidator validator;
+
+    @RequestMapping(value={"/user/login", "/locale"}, method=RequestMethod.GET)
+	public String execute(Model model) {
+        if (!model.containsAttribute("userinfo")) {
+            model.addAttribute("userinfo", new UserInfo());
+        }
+        return "login";
+	}
+    
+    @RequestMapping(value="/validateUser", method=RequestMethod.POST)
+	public ModelAndView validate(@ModelAttribute("userinfo") UserInfo userinfo, BindingResult bindingResult){
+        validator.validate(userinfo, bindingResult);
+        
+        if(bindingResult.hasErrors()) {
+            ModelAndView mav = new ModelAndView("login");
+            mav.getModel().putAll(bindingResult.getModel());
+            
+            return mav;
+        }
+
+		return new ModelAndView("welcome");
+	}
+}
